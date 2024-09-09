5ce68f3d1760a740c48674d4f28f4052a19e88a2
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-09-09 20:04:15 +0530

    Small change in MovieItem CSS

--- netflix/src/components/MovieItem.jsx
+++ netflix/src/components/MovieItem.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { createImageUrl } from "../services/movieServices";
-import { FaHeart, FaRegHeart } from "react-icons/fa6";
-import { PiPlay, PiPlayBold } from "react-icons/pi";
-import { arrayUnion, doc, updateDoc, getDoc } from "firebase/firestore";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { PiPlayBold } from "react-icons/pi";
+import { arrayUnion, arrayRemove, doc, updateDoc, getDoc } from "firebase/firestore";
 import { db } from "../services/firebase";
 import { UserAuth } from "../context/AuthContextProvider";
 
@@ -16,7 +16,7 @@ function MovieItem({ movie }) {
 			setLike(isFavourite);
 		};
 		fetchData();
-	}, [movie]); // Trigger re-fetch when movie changes
+	}, [movie]);
 
 	const markFavShow = async () => {
 		const userEmail = user?.email;
@@ -25,10 +25,31 @@ function MovieItem({ movie }) {
 			return;
 		}
 		const userDoc = doc(db, "users", userEmail);
-		setLike(!like); // Update state immediately for user feedback
-		await updateDoc(userDoc, {
-			favs: arrayUnion({ ...movie }),
-		});
+		try {
+			await updateDoc(userDoc, {
+				favs: arrayUnion({ ...movie }),
+			});
+			setLike(true);
+		} catch (error) {
+			console.error("Error updating favorite movie:", error);
+		}
+	};
+
+	const removeFavShow = async () => {
+		const userEmail = user?.email;
+		if (!userEmail) {
+			alert("Login to remove");
+			return;
+		}
+		const userDoc = doc(db, "users", userEmail);
+		try {
+			await updateDoc(userDoc, {
+				favs: arrayRemove({ ...movie }),
+			});
+			setLike(false);
+		} catch (error) {
+			console.error("Error removing favorite movie:", error);
+		}
 	};
 
 	const isFav = async (movie) => {
@@ -37,59 +58,45 @@ function MovieItem({ movie }) {
 			if (userEmail) {
 				const userDoc = doc(db, "users", userEmail);
 				const userSnap = await getDoc(userDoc);
-				if (userSnap.exists) {
+				if (userSnap.exists()) {
 					const userData = userSnap.data();
 					if (userData.favs) {
-						const isFavorite = userData.favs.some(
-							(fav) => fav.title === movie.title
-						);
-						return isFavorite;
+						return userData.favs.some((fav) => fav.title === movie.title);
 					}
 				}
 			}
 		} catch (err) {
 			console.error(err);
-			return false;
 		}
 		return false;
 	};
 
 	return (
-		<>
-			<div className=" relative  flex flex-none flex-col rounded">
-				<div className="">
-					<div className="w-72">
-						<img
-							className=" w-72 rounded mb-2 object-cover "
-							src={createImageUrl(movie.backdrop_path, "original")}
-							alt={movie.title}
-						></img>
-					</div>
-					<p className="absolute left-32 top-16 opacity-25 hover:opacity-100">
-						<PiPlayBold size={32} color="white" className="" />
-					</p>
-				</div>
-				<div className=" ">
-					<h1 className=" font-semibold">{movie.title}</h1>
-				</div>
-				<p
-					onClick={markFavShow}
-					className=" absolute top-0 right-0 mr-2 mt-2  "
-				>
-					{isFav ? (
-						<FaHeart
-							onClick={() => setLike(false)}
-							className=" opacity-0 hover:text-xl hover:opacity-100"
-						/>
-					) : (
-						<FaRegHeart
-							onClick={() => setLike(true)}
-							className=" opacity-0 hover:text-xl hover:opacity-100"
-						/>
-					)}
+		<div className="relative flex flex-none flex-col rounded group overflow-hidden">
+			<div className="relative w-72 h-40">
+				<img
+					className="w-full h-full object-cover  group-hover:scale-110"
+					src={createImageUrl(movie.backdrop_path, "original")}
+					alt={movie.title}
+				/>
+				<p className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-25 group-hover:opacity-100">
+					<PiPlayBold size={32} color="white" />
 				</p>
 			</div>
-		</>
+			<div className="p-2">
+				<h1 className="font-semibold text-center">{movie.title}</h1>
+			</div>
+			<p
+				onClick={like ? removeFavShow : markFavShow}
+				className="absolute top-2 right-2 cursor-pointer opacity-0 group-hover:opacity-100"
+			>
+				{like ? (
+					<FaHeart size={24} color="red" />
+				) : (
+					<FaRegHeart size={24} />
+				)}
+			</p>
+		</div>
 	);
 }
 
